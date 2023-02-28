e1d96588891049fe1f29f01eddd8b7a8a6d4cda4
Diogotb <digogo@gmail.com>
2023-02-28 17:29:54 -0300

    23/02/23

---
+++ CalculadoraDescontos/CalculadoraDescontos.java
@@ -0,0 +1,31 @@
+package CalculadoraDescontos;
+
+import java.util.Scanner;
+
+public class CalculadoraDescontos {
+    Scanner sc = new Scanner(System.in);
+    public void calcular20(){
+        
+        System.out.println("Informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        final int desconto20 = 20;
+        double precoComDesconto = precoProduto
+        -(precoProduto*desconto20/100);
+        System.out.println("O Preço com Desconto é "
+        + precoComDesconto + " reais");
+        
+
+    }
+    public void calcular30(){
+        
+        System.out.println("Informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        final int desconto30 = 30;
+        double precoComDesconto = precoProduto
+        -(precoProduto*desconto30/100);
+        System.out.println("O Preço com Desconto é "
+        + precoComDesconto + " reais");
+        
+
+    }
+}
---
+++ Operadores/App.java
@@ -0,0 +1,11 @@
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Operacoes objeto1 = new Operacoes();
+        objeto1.aritmetica();
+        objeto1.atribuicao();
+    }
+}
