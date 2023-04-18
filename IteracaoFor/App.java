d053d523cf0fd58df79ac487a8ccf0201370bf53
Diogo Takamori Barbosa <42560225+Diogotb@users.noreply.github.com>
2023-04-18 22:12:49 +0000

    18/04

--- IteracaoFor/App.java
+++ IteracaoFor/App.java
@@ -2,8 +2,8 @@ package IteracaoFor;
 
 public class App {
     public static void main(String[] args) {
-       Exercicios obj = new Exercicios();
-       obj.exercicio5();
+       ExemploFor obj = new ExemploFor();
+       obj.exemplo5();
         
     }
 }
