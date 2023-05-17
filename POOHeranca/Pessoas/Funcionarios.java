085bfa4f6f169e148211f36b8255d5d01f9379e6
Diogotb <digogo@gmail.com>
2023-05-16 22:00:30 -0300

    16/05

---
+++ POOHeranca/Pessoas/Funcionarios.java
@@ -0,0 +1,21 @@
+package POOHeranca.Pessoas;
+
+public class Funcionarios extends Pessoas {
+    //atributos específicos
+    int nif;
+    String setor;
+    //métodos
+    public int getNif() {
+        return nif;
+    }
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+    public String getSetor() {
+        return setor;
+    }
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+    
+}
---
+++ POOHeranca/Pessoas/Professores.java
@@ -0,0 +1,15 @@
+package POOHeranca.Pessoas;
+
+public class Professores extends Funcionarios{
+    //atributo especifico
+    private String diciplina;
+    //metodos
+    public String getDiciplina() {
+        return diciplina;
+    }
+
+    public void setDiciplina(String diciplina) {
+        this.diciplina = diciplina;
+    }
+    
+}
