9de2f0c2e8b426331f330382e36075f0590f50d5
Diogotb <digogo@gmail.com>
2023-07-25 21:23:23 -0300

    26/07/23

---
+++ RevisaoFPOO/App.java
@@ -0,0 +1,13 @@
+package RevisaoFPOO;
+
+import RevisaoFPOO.Cadastro.Animal;
+
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //Animal obj1 = new Animal("Peixinho", 2, "Gato", "Diogo");
+    }
+}
