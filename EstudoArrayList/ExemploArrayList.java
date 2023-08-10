fc292c4526b2e19bbd99df6376d6a263a72dd601
Diogotb <digogo@gmail.com>
2023-08-10 19:34:48 -0300

    10/08

--- EstudoArrayList/ExemploArrayList.java
+++ EstudoArrayList/ExemploArrayList.java
@@ -1,6 +1,6 @@
 package EstudoArrayList;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import javax.swing.JOptionPane;
 
@@ -11,6 +11,7 @@ public class ExemploArrayList {
     //atributo
     //Declarando o ArrayList do Tipo String
     ArrayList<String> carros = new ArrayList<>();
+    ArrayList<Integer> num = new ArrayList<>();
     //métodos
     public void teste() {
         //Adicionar Valores ao ArrayList
@@ -21,12 +22,16 @@ public class ExemploArrayList {
         carros.add("Gol");
         //Imprimir toda a Lista
         System.out.println(carros);
+        //Ordenando a Lista
+        Collections.sort(carros);
         //pegando o valor correspondente a 5 posição(index 4)
         System.out.println(carros.get(4));
         //percorrendo a lista com FOR
         for (int i = 0; i < carros.size(); i++) {
             System.out.println(carros.get(i));
         }
+        //Collection.reverse(invertendo as listas)
+        Collections.reverse(carros);
         //percorrendo a lista com o FOR-EACH
         for (String i : carros) {
             System.out.println(i);
@@ -37,6 +42,13 @@ public class ExemploArrayList {
     }
     public void exercicio() {
         //criar um arraylist (Integer) + 5 valores
+        //preencher 5 nº int
+        for (int i = 0; i < 5; i++) {
+            num.add(Integer.parseInt(JOptionPane.showInputDialog("Digite um nº")));
+        }
+        for (int i : num) {
+            System.out.println(i);
+        }
     }
     
 }
